b3ed96fb1e6d839be282984084e1392969565998
chnatiuk <hnatiuk.conner@gmail.com>
2021-08-25 21:38:38 -0600

    Working front end

--- connect.js
+++ connect.js
@@ -1,37 +1,33 @@
 'use strict'
 
 
-
-/*
-// Open the database in memory
-const sqlite3 = require('sqlite3').verbose();
-
-let db = new sqlite3.Database('./db/storage.db', (err) => {
-    if (err) {
-        console.error(err.message);
-    } else {
-        console.log('Connected to the in memory SQlite database.')
-    }
-});
-
-
-// Close the database
-db.close((err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log('Close the database connection.');
-  });
-
-*/
 document.querySelector('.search').addEventListener('click', function() {
-     console.log('search clicked');
+    document.querySelector('.search-sec').classList.remove('hidden');
+    document.querySelector('.insert').classList.add('hidden');
+    document.querySelector('.remove').classList.add('hidden');
+    document.querySelector('.btns').classList.add('bts-search-clicked');
+    document.body.style.backgroundColor = "#FFB3E9";
+    //document.querySelector('.insert').classList.add('.search-clicked');
+    //document.querySelector('.insert').style.position = 'absolute';
+    //document.querySelector('.insert').style.bottom = '-500px';
+    console.log('search clicked');
 });
 
 document.querySelector('.insert').addEventListener('click', function() {
+    document.querySelector('.insert-sec').classList.remove('hidden');
+    document.querySelector('.search').classList.add('hidden');
+    document.querySelector('.remove').classList.add('hidden');
+    document.querySelector('.btns').classList.add('bts-insert-clicked');
+    document.body.style.backgroundColor = "#BAE1C4";
     console.log('insert clicked');
 });
 
+// Remove Button Controller
 document.querySelector('.remove').addEventListener('click', function() {
+    //document.querySelector('.insert-sec').classList.remove('hidden');
+    document.querySelector('.search').classList.add('hidden');
+    document.querySelector('.insert').classList.add('hidden');
+    document.querySelector('.btns').classList.add('bts-remove-clicked');
+    document.body.style.backgroundColor = "#BAE1FF";
     console.log('remove clicked');
 });
